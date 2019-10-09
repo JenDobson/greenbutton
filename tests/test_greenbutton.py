0e3701bda3ce216bd006cc748aa6300ed54ae12c
Jen Dobson <jendobson@gmail.com>
2019-10-08 17:01:35 -0700

    Add test file

---
+++ tests/test_greenbutton.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_loads_xml(self):
+        self.fail("Finish the test!")
+    
+if __name__ == '__main__':
+    unittest.main()
+    
