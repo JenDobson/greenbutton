d6ed05642eb7cf571c3b426a00a74896d70a08f5
Jen Dobson <jendobson@gmail.com>
2019-10-15 13:26:19 -0700

    breakout scripting from module file to demo_script

---
+++ greenbutton/demo_script.py
@@ -0,0 +1,21 @@
+import greenbutton.greenbutton as gb
+import matplotlib.pyplot as plt
+
+
+
+
+plt.plot(gb.SAMPLE_DATA.Watts)
+plt.show()
+
+
+df_night_use = filter_by_time_of_day(df,datetime.time(23,0),datetime.time(5,0))
+
+# Plot use per night 11pm to 5am
+df_night_use_by_day = df_night_use.groupby(lambda x: df_night_use['Start Time'].loc[x].date()).sum()
+plt.plot(df_night_use_by_day.Watts)
+plt.show(block=False)
+
+
+# Boxplot to show distribution in hourly use across all data
+gb.boxplot_use_by_hour(gb.SAMPLE_DATA)
+
