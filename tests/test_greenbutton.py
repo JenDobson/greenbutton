77f9e7f787aabccfb7ccacf844c9337b5d71261b
Jen Dobson <jendobson@gmail.com>
2019-10-15 13:34:37 -0700

    update test

--- tests/test_greenbutton.py
+++ tests/test_greenbutton.py
@@ -9,8 +9,6 @@ class TestStringMethods(unittest.TestCase):
     def test_has_sample_data(self):
         self.assertTrue(isinstance(gb.SAMPLE_DATA,pd.DataFrame))
         
-        self.fail("Finish the test!")
-        
     def test_create_box_plot_of_use_by_hour(self):
         ax = gb.boxplot_use_by_hour(gb.SAMPLE_DATA)
         self.assertIsNotNone(ax)
